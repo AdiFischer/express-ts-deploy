153b6942218e46a7b8a2241e734c53fc94c9c9ba
AdiFischer <adifischer1@gmail.com>
2022-11-12 18:31:11 -0500

    added more routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,22 +2,27 @@ import * as functions from "firebase-functions";
 import * as express from "express";
 import {Request, Response} from "express";
 import * as cors from "cors";
+import { createNewPost, getAllPosts, updatePost, deletePost, getOnePost } from './posts'
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response): void => {
-  res.send("welcome to TS");
+  res.send("welcome to ADI");
 });
+
+app.get("/new", (req: Request, res: Response): void => {
+  res.send("welcome to js");
+});
+
+app.get('/posts', getAllPosts)
+app.post('/post', createNewPost)
+app.patch('/post/:uid', updatePost)
+app.delete('/post/:uid', deletePost)
+app.get('/post/:uid', getOnePost)
+
 
 
 export const api = functions.https.onRequest(app);
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
---
+++ functions/src/posts.ts
@@ -0,0 +1,55 @@
+import dbConnect from './dbConnect'
+import { Request, Response } from 'firebase-functions/v1'
+
+
+export function createNewPost(req: Request, res: Response) {
+    const db = dbConnect()
+    db.collection('posts')
+    .add(req.body)
+      //.then(() => getAllPosts(req, res))
+      .then(newPost => res.status(201).send({ success: true, message: 'post created: ' + newPost.id}))
+      .catch(err => res.status(500).send({ success: false, message: err }))
+  }
+
+  export function getAllPosts(req: Request, res: Response) {
+    const db = dbConnect()
+    db.collection('posts').get()
+      .then((collection: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>) => {
+        console.log(collection.size)
+        const postsArr = collection.docs.map((doc: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>) => {
+          return { ...doc.data(), postId: doc.id }
+        })
+        res.status(200).send(postsArr)
+      })
+      .catch(err => res.status(500).send({ success: false, message: err }))
+  }
+
+  export async function updatePost(req: Request, res: Response) {
+    const { uid } = req.params
+    const db = dbConnect()
+    await db.collection('posts').doc(uid).update(req.body)
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'post Updated ' + uid })
+  }
+
+  export async function deletePost(req: Request, res: Response) {
+    const { uid } = req.params
+    const db = dbConnect()
+   await db.collection('posts').doc(uid).delete()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'post Deleted' })
+  }
+  
+  export async function getOnePost(req: Request, res: Response) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('posts').doc(uid).get()
+    if (!doc.exists){
+        console.log("no match")
+        res.status(404).send({ success: false, message: 'not found'})
+
+    } else{
+        res.status(202).send({ success: true, message: doc.data()})
+
+    }
+}
